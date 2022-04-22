4260c2677f3cef56cdae021c9abbca7b41d3c770
JulioSD26 <JulioSanchezD26@gmail.com>
2022-04-21 23:31:33 -0700

    Initial commit

---
+++ src/main/java/Practica1.java
@@ -0,0 +1,11 @@
+public class Practica1 {
+    public static void main(String[] args) {
+        for (String s:
+                args) {
+            int n = Integer.parseInt(s);
+
+           // Thread t = new Thread(new Factorial(n));
+            //t.start();
+        }
+    }
+}
